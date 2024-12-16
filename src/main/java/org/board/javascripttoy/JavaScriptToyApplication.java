6fb8c0de3362eb59d6526a842c54d6b2fa8dc3f1
BangCat <cuhng6063@naver.com>
2024-12-16 16:07:53 +0900

    게임

---
+++ src/main/java/org/board/javascripttoy/JavaScriptToyApplication.java
@@ -0,0 +1,13 @@
+package org.board.javascripttoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaScriptToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaScriptToyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/board/javascripttoy/JavaScriptToyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.board.javascripttoy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaScriptToyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
